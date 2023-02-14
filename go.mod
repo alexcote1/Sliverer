35efe4069cc81c449025781eecb1900d882db175
ALEXANDER MA COTE <alexcote1rocks@gmail.com>
2023-02-14 16:33:04 -0500

    intial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/alexcote1/Sliverer
+
+go 1.20
+
+require github.com/bishopfox/sliver v1.15.16
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210722135532-667f2b7c528f // indirect
+	google.golang.org/grpc v1.42.0-dev.0.20211020220737-f00baa6c3c84 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/bishopfox/sliver/client/assets"
+	consts "github.com/bishopfox/sliver/client/constants"
+	"github.com/bishopfox/sliver/client/transport"
+	"github.com/bishopfox/sliver/protobuf/clientpb"
+	"github.com/bishopfox/sliver/protobuf/commonpb"
+	"github.com/bishopfox/sliver/protobuf/rpcpb"
+	"github.com/bishopfox/sliver/protobuf/sliverpb"
+)
+
+func makeRequest(session *clientpb.Session) *commonpb.Request {
+	if session == nil {
+		return nil
+	}
+	timeout := int64(60)
+	return &commonpb.Request{
+		SessionID: session.ID,
+		Timeout:   timeout,
+	}
+}
+func main() {
+	var configPath string
+	var command string
+	var runonnew bool
+	flag.StringVar(&configPath, "config", "", "path to sliver client config file")
+	flag.StringVar(&command, "command", "", "command to run")
+	flag.BoolVar(&runonnew, "runonnew", false, "weather or not to run on all new agents, hangs by default")
+	flag.Parse()
+	if configPath == "" {
+		println("no config is provided --config would work, but attempting to guess based on whats in ~/.sliver-client/configs/")
+		files, err := ioutil.ReadDir(os.Getenv("HOME") + "/.sliver-client/configs/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		configPath = os.Getenv("HOME") + "/.sliver-client/configs/" + files[0].Name()
+
+	}
+	// load the client configuration from the filesystem
+	config, err := assets.ReadConfig(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// connect to the server
+	rpc, ln, err := transport.MTLSConnect(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("[*] Connected to sliver server")
+	defer ln.Close()
+	//command = "ls"
+	if runonnew != true {
+		runcommandonall(rpc, command)
+	} else {
+		runcommandonnew(rpc, command)
+	}
+
+}
+func runcommandonall(rpc rpcpb.SliverRPCClient, command string) {
+	agents, err := rpc.GetSessions(context.Background(), &commonpb.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	//print(agents)
+	for i := 0; i < len(agents.Sessions); i++ {
+		//print(&agents.Sessions[i])
+		if agents.Sessions[i].IsDead == true {
+			println(agents.Sessions[i].Hostname + " is dead")
+		} else {
+			//println(i)
+			runcommandon(rpc, command, agents.Sessions[i])
+		}
+	}
+}
+func runcommandon(rpc rpcpb.SliverRPCClient, command string, agent *clientpb.Session) {
+	resp, err := rpc.Execute(context.Background(), &sliverpb.ExecuteReq{
+		Path:    command,
+		Output:  true,
+		Request: makeRequest(agent),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	println(agent.Hostname)
+	println(string(resp.Stdout) + string(resp.Stderr))
+}
+func runcommandonnew(rpc rpcpb.SliverRPCClient, command string) {
+	// Open the event stream to be able to collect all events sent by  the server
+	eventStream, err := rpc.Events(context.Background(), &commonpb.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// infinite loop
+	for {
+		event, err := eventStream.Recv()
+		if err == io.EOF || event == nil {
+			return
+		}
+		// Trigger event based on type
+		switch event.EventType {
+
+		// a new session just came in
+		case consts.SessionOpenedEvent:
+			session := event.Session
+			// call any RPC you want, for the full list, see
+			// https://github.com/BishopFox/sliver/blob/master/protobuf/rpcpb/services.proto
+			runcommandon(rpc, command, session)
+		}
+	}
+}
