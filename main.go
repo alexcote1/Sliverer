1a05d27f4b870c3b4debdbafde656d261b696ee3
ALEXANDER MA COTE <alexcote1rocks@gmail.com>
2023-03-07 15:24:29 -0500

    added a bunch of stuff

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"container/list"
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -18,6 +19,7 @@ import (
 	"github.com/bishopfox/sliver/protobuf/commonpb"
 	"github.com/bishopfox/sliver/protobuf/rpcpb"
 	"github.com/bishopfox/sliver/protobuf/sliverpb"
+	"google.golang.org/protobuf/proto"
 )
 
 type task struct {
@@ -46,18 +48,27 @@ func makeBeaconRequest(beacon *clientpb.Beacon) *commonpb.Request {
 		Async:    true,
 	}
 }
+
 func main() {
 	var configPath string
 	var command string
 	var runonnew bool
 	var argss string
+	var rename bool
+	var listbeacons bool
+	var hostss string
 	flag.StringVar(&configPath, "config", "", "path to sliver client config file")
 	flag.StringVar(&command, "command", "", "command to run")
 	flag.StringVar(&argss, "args", "", "command args")
 	flag.BoolVar(&runonnew, "runonnew", false, "weather or not to run on all new agents, hangs by default")
+	flag.BoolVar(&rename, "rename", false, "run a rename operation")
+	flag.BoolVar(&listbeacons, "listbeacons", false, "get a list of all beacon names")
+	flag.StringVar(&hostss, "hosts", "", "runs command on list of hosts")
 	flag.Parse()
 	var args []string
+	var hosts []string
 	args = strings.Split(argss, "^")
+	hosts = strings.Split(hostss, " ")
 	if configPath == "" {
 		println("no config is provided --config would work, but attempting to guess based on whats in ~/.sliver-client/configs/")
 		files, err := ioutil.ReadDir(os.Getenv("HOME") + "/.sliver-client/configs/")
@@ -81,13 +92,59 @@ func main() {
 	log.Println("[*] Connected to sliver server")
 	defer ln.Close()
 	//command = "ls"
-	if runonnew != true {
-		runcommandonall(rpc, command, args)
-	} else {
+	if rename == true {
+		renameall(rpc)
+	} else if runonnew == true {
 		runcommandonnew(rpc, command, args)
+	} else if listbeacons == true {
+		getbeacons(rpc)
+	} else if len(hosts) > 0 {
+		runcommandonlist(rpc, command, args, hosts)
+	} else {
+		runcommandonall(rpc, command, args)
+
 	}
 
 }
+
+func runcommandonlist(rpc rpcpb.SliverRPCClient, command string, args []string, hosts []string) {
+	beacons, err := rpc.GetBeacons(context.Background(), &commonpb.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	runon := []*clientpb.Beacon{}
+
+	for i := 0; i < len(beacons.Beacons); i++ {
+		if isinarray(hosts, beacons.Beacons[i].Name) {
+			runon = append(runon, beacons.Beacons[i])
+		}
+	}
+	//print(&agents.Sessions[i])
+	//println(i)
+	beacons.Beacons = runon
+	runonbeacons(beacons, rpc, command, args)
+
+}
+func isinarray(hosts []string, host string) bool {
+	for _, element := range hosts {
+		if element == host {
+			return true
+		}
+	}
+	return false
+
+}
+func getbeacons(rpc rpcpb.SliverRPCClient) {
+	beacons, err := rpc.GetBeacons(context.Background(), &commonpb.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < len(beacons.Beacons); i++ {
+		fmt.Fprintf(os.Stdout, beacons.Beacons[i].Name+"\n")
+	}
+}
+
 func runcommandonall(rpc rpcpb.SliverRPCClient, command string, args []string) {
 	agents, err := rpc.GetSessions(context.Background(), &commonpb.Empty{})
 	if err != nil {
@@ -107,13 +164,138 @@ func runcommandonall(rpc rpcpb.SliverRPCClient, command string, args []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//print(&agents.Sessions[i])
+	//println(i)
+	runonbeacons(beacons, rpc, command, args)
+}
+func renameall(rpc rpcpb.SliverRPCClient) {
+	// agents, err := rpc.GetSessions(context.Background(), &commonpb.Empty{})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for i := 0; i < len(agents.Sessions); i++ {
+	// 	ifconfig, err := rpc.Ifconfig(context.Background(), &sliverpb.IfconfigReq{
+	// 		Request: makeRequest(agents.Sessions[i]),
+	// 	})
+	// 	if err != nil {
+	// 		log.Print(err)
+	// 	}
+	// 	print(ifconfig)
+	// }
+
+	beacons, err := rpc.GetBeacons(context.Background(), &commonpb.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
 	taskids := list.New()
 	for i := 0; i < len(beacons.Beacons); i++ {
-		//print(&agents.Sessions[i])
 		if beacons.Beacons[i].IsDead == true {
 			println(beacons.Beacons[i].Hostname + " is dead")
 		} else {
-			//println(i)
+
+			ifconfig, err := rpc.Ifconfig(context.Background(), &sliverpb.IfconfigReq{
+				Request: makeBeaconRequest(beacons.Beacons[i]),
+			})
+			if err != nil {
+				log.Print(err)
+			}
+			taskids.PushFront(task{taskid: ifconfig.Response.TaskID, beacon: beacons.Beacons[i]})
+		}
+	}
+
+	for k := 0; k < 100; k++ {
+		if taskids.Front() == nil {
+			continue
+		}
+		log.Println("waiting 10 seconds")
+		time.Sleep(10 * time.Second)
+		for i := taskids.Front(); i != nil; i = i.Next() {
+
+			tasks, err := rpc.GetBeaconTasks(context.Background(), (i.Value).(task).beacon)
+
+			if err != nil {
+				log.Print(err)
+			}
+			for j := 0; j < len(tasks.Tasks); j++ {
+				if tasks.Tasks[j].State == "completed" {
+					if tasks.Tasks[j].ID == (i.Value).(task).taskid {
+						resp, err := rpc.GetBeaconTaskContent(context.Background(), tasks.Tasks[j])
+						if err != nil {
+							log.Print(err)
+						}
+						oldval := i.Value
+						old := i.Prev()
+						if old == nil {
+							println("HELP")
+							if i.Next() == nil {
+								taskids.Remove(i)
+								i = nil
+							} else {
+								taskids.MoveToFront(i.Next())
+								taskids.Remove(i)
+								i = taskids.Front()
+							}
+
+						} else {
+							taskids.Remove(i)
+							i = old
+						}
+
+						ifconfig := &sliverpb.Ifconfig{}
+						err = proto.Unmarshal(resp.Response, ifconfig)
+						if err != nil {
+							log.Print("Failed to decode task response: %s\n", err)
+
+						}
+
+						println((oldval).(task).beacon.Name + "," + (oldval).(task).beacon.Hostname)
+						for g := 0; g < len(ifconfig.NetInterfaces); g++ {
+							if ifconfig.NetInterfaces[g].Name != "lo" {
+								for k := 0; k < len(ifconfig.NetInterfaces[g].IPAddresses); k++ {
+									if !strings.Contains(ifconfig.NetInterfaces[g].IPAddresses[k], ":") {
+										println(ifconfig.NetInterfaces[g].IPAddresses[k])
+										ipaddr := ifconfig.NetInterfaces[g].IPAddresses[k]
+										ipaddr = strings.Split(ipaddr, "/")[0]
+										name := ipaddr + "_" + (oldval).(task).beacon.Hostname + "."
+										println(name)
+										_, err := rpc.Rename(context.Background(), &clientpb.RenameReq{
+											BeaconID: (oldval).(task).beacon.ID,
+											Name:     name,
+										})
+
+										if err != nil {
+											log.Print("Failed to decode task response: %s\n", err)
+
+										}
+									}
+								}
+							}
+						}
+						//println(string(resp.Response))
+					}
+				}
+			}
+			if i == nil {
+				println("i think i got everyone")
+				break
+			}
+		}
+
+	}
+	for i := taskids.Front(); i != nil; i = i.Next() {
+		println("didnt hear from " + (i.Value).(task).beacon.Name + "," + (i.Value).(task).beacon.Hostname)
+	}
+
+}
+
+func runonbeacons(beacons *clientpb.Beacons, rpc rpcpb.SliverRPCClient, command string, args []string) {
+	taskids := list.New()
+	for i := 0; i < len(beacons.Beacons); i++ {
+
+		if beacons.Beacons[i].IsDead == true {
+			println(beacons.Beacons[i].Hostname + " is dead")
+		} else {
+
 			taskids.PushFront(runcommandonbeacon(rpc, command, beacons.Beacons[i], args))
 		}
 	}
@@ -121,6 +303,7 @@ func runcommandonall(rpc rpcpb.SliverRPCClient, command string, args []string) {
 		if taskids.Front() == nil {
 			continue
 		}
+		log.Println("waiting 10 seconds")
 		time.Sleep(10 * time.Second)
 		for i := taskids.Front(); i != nil; i = i.Next() {
 			tasks, err := rpc.GetBeaconTasks(context.Background(), (i.Value).(task).beacon)
@@ -134,13 +317,40 @@ func runcommandonall(rpc rpcpb.SliverRPCClient, command string, args []string) {
 						if err != nil {
 							log.Print(err)
 						}
-						taskids.Remove(i)
-						println((i.Value).(task).beacon.Name + "," + (i.Value).(task).beacon.Hostname)
-						println(string(resp.Response))
+						oldval := i.Value
+						old := i.Prev()
+						if old == nil {
+							println("HELP")
+							if i.Next() == nil {
+								taskids.Remove(i)
+								i = nil
+							} else {
+								taskids.MoveToFront(i.Next())
+								taskids.Remove(i)
+								i = taskids.Front()
+							}
+
+						} else {
+							taskids.Remove(i)
+							i = old
+						}
+
+						println((oldval).(task).beacon.Name + "," + (oldval).(task).beacon.Hostname)
+						command := &sliverpb.Execute{}
+						err = proto.Unmarshal(resp.Response, command)
+						if err != nil {
+							log.Print("Failed to decode task response: %s\n", err)
+
+						}
+						println(string(command.Stdout))
+						println(string(command.Stderr))
 					}
 				}
 			}
-
+			if i == nil {
+				println("i think i got everyone")
+				break
+			}
 		}
 	}
 	for i := taskids.Front(); i != nil; i = i.Next() {
